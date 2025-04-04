84f9c610c3183eef14d37b326658c96cb99b0483
Karina <harisova_k@mail.ru>
2025-04-04 20:07:48 +0500

    adding initial configs

---
+++ mmorpg_board/mmorpg_board/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for mmorpg_board project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django_ckeditor_5.views import upload_file
+
+from mmorpg_board import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path("", include("board.urls")),
+    path("ckeditor5/upload/", upload_file, name="ck_editor_5_upload_file"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
